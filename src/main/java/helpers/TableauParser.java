0910c54ef8330de53dd4f2b93c7a2c933a04dc8f
Alexander Nyström <dv15anm@cs.umu.se>
2016-12-20 16:33:46 +0100

    It now loops all pages from the api
    It now only fetches the data that is within the 24h interval

--- src/main/java/helpers/TableauParser.java
+++ src/main/java/helpers/TableauParser.java
@@ -57,16 +57,25 @@ public class TableauParser {
             URL yesterday = new URL(apiUrl+getYesterday());
             URL tomorrow = new URL(apiUrl+getTomorrow());
             URL today = new URL(apiUrl);
+            System.out.println(yesterday+"\n"+today+"\n"+tomorrow);
             InputStream todayInput = today.openStream();
-            todayDoc = parser.parse(todayInput);
             ydayDoc = parser.parse(yesterday.openStream());
+            tableaus = buildTableau(tableaus,ydayDoc);
+            while ((yesterday = getNextPage(ydayDoc)) != null) {
+                ydayDoc = parser.parse(yesterday.openStream());
+                tableaus = buildTableau(tableaus, ydayDoc);
+            }
+            todayDoc = parser.parse(todayInput);
+            tableaus = buildTableau(tableaus,todayDoc);
+            while ((today = getNextPage(todayDoc)) != null) {
+                todayDoc = parser.parse(today.openStream());
+                tableaus = buildTableau(tableaus, todayDoc);
+            }
             tomDoc = parser.parse(tomorrow.openStream());
-            if (todayDoc != null && tomDoc != null && ydayDoc != null) {
-                tableaus = buildTableau(tableaus,ydayDoc);
-                tableaus = buildTableau(tableaus,todayDoc);
-                tableaus = buildTableau(tableaus,tomDoc);
-            } else {
-                System.out.println("Could not parse");
+            tableaus = buildTableau(tableaus,tomDoc);
+            while ((tomorrow = getNextPage(tomDoc)) != null) {
+                tomDoc = parser.parse(tomorrow.openStream());
+                tableaus = buildTableau(tableaus, tomDoc);
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -87,18 +96,23 @@ public class TableauParser {
      */
     private ArrayList<Tableau> buildTableau(ArrayList<Tableau> tableaus, Document doc) {
         try {
-            int channelCount = Integer.parseInt(path.evaluate(
+            int episodeCount = Integer.parseInt(path.evaluate(
                     "count(/sr/schedule/scheduledepisode)", doc));
-            for (int i = 0; i < channelCount; i++){
+            Calendar rightNow = Calendar.getInstance();
+//            System.out.println("channel count: "+episodeCount);
+            for (int i = 0; i < episodeCount; i++){
                 Tableau tableau = new Tableau();
-                Calendar rightNow = Calendar.getInstance();
                 Calendar startTime = DatatypeConverter.parseDateTime(
-                        path.evaluate("/sr/schedule/scheduledepisode/" +
+                        path.evaluate("/sr/schedule/scheduledepisode["+(i+1)+"]/" +
                                 "starttimeutc",doc));
+//                System.out.println(startTime.get(Calendar.HOUR)+":"+startTime.get(Calendar.MINUTE)+":"+startTime.get(Calendar.SECOND));
                 if (isWithinInterval(rightNow, startTime)) {
-                    tableau.setEpisodeId(Integer.parseInt(path.evaluate(
+                    String id = path.evaluate(
                             "/sr/schedule/scheduledepisode["+(i+1)+
-                                    "]/episodeid", doc)));
+                                    "]/episodeid", doc);
+                    if (id.compareTo("") != 0) {
+                        tableau.setEpisodeId(Integer.parseInt(id));
+                    }
 
                     tableau.setTitle(path.evaluate("/sr/schedule/" +
                             "scheduledepisode["+(i+1)+ "]/title", doc));
@@ -155,16 +169,13 @@ public class TableauParser {
      * Checks if the given time is within a 24h interval of current time.
      * That is if compareTo is 12h behind or before current time
      * @param rightNow current time
-     * @param compareTo time to compare to
+     * @param startTime time to compare to
      * @return true if the time is withing the interval
      */
-    private boolean isWithinInterval(Calendar rightNow, Calendar compareTo) {
-        return ((rightNow.getTimeInMillis() -
-                TimeUnit.HOURS.toMillis(12) <=
-                compareTo.getTimeInMillis()) ||
-                (rightNow.getTimeInMillis() -
-                        TimeUnit.HOURS.toMillis(12)
-                        >= compareTo.getTimeInMillis()));
+    private boolean isWithinInterval(Calendar rightNow, Calendar startTime) {
+        return Math.abs(startTime.getTimeInMillis() -
+                rightNow.getTimeInMillis()) <=
+                TimeUnit.HOURS.toMillis(12);
     }
 
     /**
@@ -193,6 +204,21 @@ public class TableauParser {
                 tomorrow.get(Calendar.MONTH) + "-" +
                 tomorrow.get(Calendar.DATE);
         return to;
+    }
+
+    private URL getNextPage(Document doc) {
+        try {
+            String url = path.evaluate("/sr/pagination/nextpage",doc);
+//            System.out.println("Next page: "+url);
+            if (url.compareTo("") != 0){
+                return new URL(url);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void main (String[] args) {
